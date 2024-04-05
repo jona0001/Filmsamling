0ef15f0f50c216b619fc432be44a6f5defdf4ee7
KrisK <37834268+KristofferKay@users.noreply.github.com>
2024-04-05 15:05:24 +0200

    Fixed loadMovieFromFile so it now loads correctly based on the last changes we did for the saveListOfMovies.

--- src/datasource/FileHandler.java
+++ src/datasource/FileHandler.java
@@ -15,29 +15,27 @@ public class FileHandler {
             out.println(movie.toCSV());
         }
     }
-
     public ArrayList<Movie> loadMovieFromFile() {
         File moviesDB = new File("movies.csv");
         ArrayList<Movie> moviesFromCSVArr = new ArrayList();
-        Scanner sc = null;
+        Scanner sc;
         try {
             sc = new Scanner(moviesDB);
             sc.nextLine();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        Movie addMovieToDB = null;
         while (sc.hasNext()) {
-            String[] attributes = sc.nextLine().split(",");
-            addMovieToDB = new Movie(
+            String line = sc.nextLine();
+            String[] attributes = line.split(",");
+            Movie movies = new Movie(
                     (attributes[0]), // title
                     (attributes[1]), // director
                     (Integer.parseInt(attributes[2])), // year created
                     (Integer.parseInt(attributes[3])), // isInColor
                     (Boolean.parseBoolean(attributes[4])), // length
                     (attributes[5])); // genre
-
-
+            moviesFromCSVArr.add(movies);
         }
         sc.close();
         return moviesFromCSVArr;
