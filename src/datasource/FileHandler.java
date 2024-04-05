13a6f25c09699556b26b9f9c4c263308a06b2cbc
KrisK <37834268+KristofferKay@users.noreply.github.com>
2024-04-05 09:25:39 +0200

    Datasource package added FileHandler added datasource package Deleted stuff in movies.csv

---
+++ src/datasource/FileHandler.java
@@ -0,0 +1,48 @@
+package datasource;
+import domain.Movie;
+import domain.Controller;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileHandler {
+
+    Controller controller = new Controller();
+    void saveListOfMovies() throws FileNotFoundException {
+        PrintStream out = new PrintStream(new FileOutputStream(("movies.csv"), true));
+        for (Movie movie : controller.addMovie()) {
+            out.println(movie.toCSV());
+        }
+    }
+
+    public ArrayList<Movie> loadMovieFromFile() {
+        File moviesDB = new File("movies.csv");
+        ArrayList<Movie> moviesFromCSVArr = new ArrayList();
+        Scanner sc = null;
+        try {
+            sc = new Scanner(moviesDB);
+            sc.nextLine();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Movie addMovieToDB = null;
+        while (sc.hasNext()) {
+            String[] attributes = sc.nextLine().split(",");
+            addMovieToDB = new Movie(
+                    (attributes[0]), // title
+                    (attributes[1]), // director
+                    (Integer.parseInt(attributes[2])), // year created
+                    (Integer.parseInt(attributes[3])), // isInColor
+                    (Boolean.parseBoolean(attributes[4])), // length
+                    (attributes[5])); // genre
+
+            filmListe.add(addMovieToDB);
+        }
+        sc.close();
+        return moviesFromCSVArr;
+    }
+
+}
--- src/domain/MovieCollection.java
+++ src/domain/MovieCollection.java
@@ -89,38 +89,5 @@ public class MovieCollection {
         return isDeleted;
     }
 
-    void saveListOfMovies() throws FileNotFoundException {
-        PrintStream out = new PrintStream(new FileOutputStream(("movies.csv"), true));
-        for (Movie movie : filmListe) {
-            out.println(movie.toCSV());
-        }
-    }
-
-    public ArrayList<Movie> loadMovieFromFile() {
-        File moviesDB = new File("movies.csv");
-        ArrayList<Movie> moviesFromCSVArr = new ArrayList();
-        Scanner sc = null;
-        try {
-            sc = new Scanner(moviesDB);
-            sc.nextLine();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        Movie addMovieToDB = null;
-        while (sc.hasNext()) {
-            String[] attributes = sc.nextLine().split(",");
-            addMovieToDB = new Movie(
-                (attributes[0]), // title
-                (attributes[1]), // director
-                (Integer.parseInt(attributes[2])), // year created
-                (Integer.parseInt(attributes[3])), // isInColor
-                (Boolean.parseBoolean(attributes[4])), // length
-                (attributes[5])); // genre
-
-                filmListe.add(addMovieToDB);
-        }
-        sc.close();
-        return moviesFromCSVArr;
-    }
 }
 
