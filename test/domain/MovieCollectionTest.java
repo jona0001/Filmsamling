55e874d865330632c56e77b19ee2c3c7e56286ab
ViAarisKea <viaa0001@stud.kea.dk>
2024-04-09 10:26:29 +0200

    add unit test for add movie

--- test/domain/MovieCollectionTest.java
+++ test/domain/MovieCollectionTest.java
@@ -1,31 +1,35 @@
-package test2;
+package domain;
 
 import domain.MovieCollection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MovieCollectionTest {
 
     @Test
     void addMovie() throws FileNotFoundException {
         //arrange
-        MovieCollection movieCollection = new MovieCollection();
-        movieCollection.addMovie("javatest", "Jonathan", 2005, 200, true, "horror");
+        MovieCollection movieCollection = new MovieCollection(new ArrayList<>());
+        boolean isAdded = movieCollection.addMovie("javatest", "Jonathan", 2005, 200, true, "horror");
         //act
         int expectedResult = 1;
         int actualResult = movieCollection.getFilmListe().size();
+
         //assert
         assertEquals(expectedResult, actualResult);
+        assertTrue(isAdded);
     }
 
     @Test
     void getFilmListe() throws FileNotFoundException {
         //Arrange
-        MovieCollection movieCollection = new MovieCollection();
+        MovieCollection movieCollection = new MovieCollection(new ArrayList<>());
         movieCollection.addMovie("javaGame", "Jonathan", 2024, 20000, true, "action");
         movieCollection.addMovie("JavaHunt", "Jonathan", 2025, 20000, true, "horror");
         movieCollection.getFilmListe(); //?
