bf13c24699f83b9c654e3e9b51764f8639077d81
ViAarisKea <viaa0001@stud.kea.dk>
2024-04-09 10:42:37 +0200

    add unit test for showAllMovies and searchMovies

--- test/domain/MovieCollectionTest.java
+++ test/domain/MovieCollectionTest.java
@@ -42,9 +42,84 @@ class MovieCollectionTest {
     }
 
     @Test
-    void testToString() {
+    void findAllMovies() throws FileNotFoundException {
+        //Arrange
+        String title = "Title";
+        String director = "Director";
+        int yearCreated = 1998;
+        boolean isInColor = true;
+        int lengthInMinutes = 88;
+        String genre = "Genre";
+
+        String title2 = "Title2";
+        String director2 = "Director2";
+        int yearCreated2 = 2005;
+        boolean isInColor2 = false;
+        int lengthInMinutes2 = 120;
+        String genre2 = "Genre2";
+
+        String title3 = "Title3";
+        String director3 = "Director3";
+        int yearCreated3 = 2015;
+        boolean isInColor3 = true;
+        int lengthInMinutes3 = 130;
+        String genre3 = "Genre3";
+
+        MovieCollection movieCollection = new MovieCollection(new ArrayList<>());
+
+        movieCollection.addMovie(title, director, yearCreated, lengthInMinutes, isInColor, genre);
+        movieCollection.addMovie(title2, director2, yearCreated2, lengthInMinutes2, isInColor2, genre2);
+        movieCollection.addMovie(title3, director3, yearCreated3, lengthInMinutes3, isInColor3, genre3);
+
+        //Act
+        ArrayList<Movie> movies = movieCollection.getFilmListe();
+        int actualSize = movies.size();
+        int expectedSize = 3;
+
+        //Assert
+        assertEquals(expectedSize, actualSize);
+        for(Movie movie : movies){
+            System.out.println(movie);
+        }
+
     }
+
     @Test
-    void movieSearch () {
+    void searchMoviesByTitle() throws FileNotFoundException {
+        //Arrange
+        String title = "Title";
+        String director = "Director";
+        int yearCreated = 1998;
+        boolean isInColor = true;
+        int lengthInMinutes = 88;
+        String genre = "Genre";
+
+        String title2 = "Title2";
+        String director2 = "Director2";
+        int yearCreated2 = 2005;
+        boolean isInColor2 = false;
+        int lengthInMinutes2 = 120;
+        String genre2 = "Genre2";
+
+        String title3 = "Title3";
+        String director3 = "Director3";
+        int yearCreated3 = 2015;
+        boolean isInColor3 = true;
+        int lengthInMinutes3 = 130;
+        String genre3 = "Genre3";
+
+        MovieCollection movieCollection = new MovieCollection(new ArrayList<>());
+
+        movieCollection.addMovie(title, director, yearCreated, lengthInMinutes, isInColor, genre);
+        movieCollection.addMovie(title2, director2, yearCreated2, lengthInMinutes2, isInColor2, genre2);
+        movieCollection.addMovie(title3, director3, yearCreated3, lengthInMinutes3, isInColor3, genre3);
+
+        //Act
+        ArrayList<Movie> foundMovie = movieCollection.movieSearch("title");
+        int actualSize = foundMovie.size();
+        int expectedSize = 3;
+
+        //Assert
+        assertEquals(expectedSize, actualSize);
     }
 }
